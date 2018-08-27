f15f102e53c503d942b2f69c47e4c82d7d19453f
emersoncloud <emerson.cloud@gmail.com>
2018-08-27 13:04:29 -0400

    Add 1.0.0-beta1-basic-implementation

---
+++ app/src/main/java/com/example/emerson/ckdemo/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.emerson.ckdemo;
+
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.camerakit.CameraKitView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraKitView cameraKitView;
+    private Button photoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraKitView = findViewById(R.id.camera);
+
+        photoButton = findViewById(R.id.photoButton);
+        photoButton.setOnClickListener(photoOnClickListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private View.OnClickListener photoOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+                @Override
+                public void onImage(CameraKitView cameraKitView, final byte[] photo) {
+                    File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+                    try {
+                        FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                        outputStream.write(photo);
+                        outputStream.close();
+                    } catch (java.io.IOException e) {
+                        e.printStackTrace();
+                        Log.e("CKDemo", "Exception in photo callback");
+                    }
+                }
+            });
+        }
+    };
+}
